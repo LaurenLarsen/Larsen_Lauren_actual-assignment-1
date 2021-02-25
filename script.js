064ec649737cadb783d4d8a180da74dfff4d4830
lmccraw123 <laurenmccraw@gmail.com>
2021-02-24 20:38:04 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,123 @@
+//Step 1
+let someMonth;
+function theMonth();
+let currentMonth;
+let summerMonth;
+let MyLibraryFunction
+
+//Step 2
+//numeric literal expression
+745
+//string literal expression
+"This is a String Literal"
+//Boolean literal expression
+True
+//null literal expression
+null
+
+//Step 3
+//2 complex/variable expressions
+let complexExpression1 = 17 * (45 / 17) + 48;
+let complexExpression2 = 354 * (32 / 89) + 77;
+
+//Step 4
+let firstName; 
+let lastName; 
+let $address;
+let $city;
+let $state;
+let zipCode;
+let _yourAge;
+let referralSource;
+let mayWeContactYou;
+
+//Step 5
+let firstName ="Your First Name"; 
+let lastName = "Your Last Name"; 
+let zipCode = "92101";
+
+//Step 6
+let yourAge = 25;
+alert("You are" + yourAge + "years old"); 
+
+//Step 7
+let variableOne = True;
+let variableTwo = variableOne + "This is your age";
+
+//Step 8
+
+//Step 9
+//null value
+let value = 1000;
+let noValue = null;
+value = null;
+console.log(value);
+
+//undefined value
+let value;
+console.log(value);
+
+//Step 10
+//string 
+typeof "Roses are Red";
+//number
+typeof 10;
+//Boolean
+typeof true;
+//object
+typeof {a:1};
+
+//undefined
+typeof value;
+
+//Step 11
+let yourName = "Zak Ruvalcaba"
+alert("Hello " + yourName + ", welcome to the JavaScript class!")
+
+//Step 12
+let yourName = "Lauren Larsen";
+alert(yourName);
+
+//Step 13
+let course = "JavaScript";
+alert(course);
+
+//Step 14
+let welcomeString ="Hello Zak Ruvalcaba \r  WElcome to the JavaScript class!";
+
+//Step 15
+prompt("what is your name");
+alert("Hello" + welcomeString + `${\r}` "Welcome to the JavaScript class!")
+
+//Step 16
+prompt("What Course are you taking?");
+alert(prompt);
+
+//Step 17
+let x = 10;
+let y = 20;
+console.log( x + y);
+
+//Step 18
+let x = 20;
+console.log( x + 20);
+
+//Step 19
+let x = 20;
+console.log(5*x);
+//Step 20
+let x = 20 % 3;
+console.log( x / 1);
+
+//Step 21
+let choiceOne = 15;
+if(choiceOne > 10){
+    alert(true);
+}
+
+//Step 22
+let choiceTwo = 25;
+let choiceThree = 33;
+if (choiceTwo === choiceThree){
+    alert(true);
+}else{alert(false);}
